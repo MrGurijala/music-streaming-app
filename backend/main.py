6a0344e303d18871a393358b5bd11464bcc9293c
Mr.Gurijala <grvsaiteja@gmail.com>
2025-03-07 00:12:06 +0000

    added lamda handler for deployment

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from mangum import Mangum 
 from routes.auth import auth_router
 
 # Initialize FastAPI app
@@ -12,6 +13,9 @@ app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 @app.get("/docs")
 def get_docs():
     return {"message": "Visit /docs for Swagger UI"}
+
+# Lambda handler for AWS Lambda deployment
+handler = Mangum(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
