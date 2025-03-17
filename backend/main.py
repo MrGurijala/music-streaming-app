609e134243bbeff03e77f3a8d8cc909b7747435f
Mr.Gurijala <grvsaiteja@gmail.com>
2025-03-17 14:34:07 +0000

    added the backend code

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 import uvicorn
 from mangum import Mangum 
 from routes.auth import auth_router
+from routes.album import albums_router
+from routes.favourite import favorites_router
+from routes.playlist import playlists_router
+from routes.songs import songs_router
 
 # Initialize FastAPI app
 app = FastAPI()
 
 # Include routes
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(albums_router, prefix="/albums", tags=["Albums"])
+app.include_router(favorites_router, prefix="/favorites", tags=["Favorites"])
+app.include_router(playlists_router, prefix="/playlists", tags=["Playlists"])
+app.include_router(songs_router, prefix="/songs", tags=["Songs"])
 
 # API Documentation
 @app.get("/docs")
